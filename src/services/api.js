cbc24146b0c85333f2740a7d82b497bac4ebb83b
Nirav <niravmathukiya8@gmail.com>
2026-05-20 13:07:01 +0530

    changethe port

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://localhost:9000"
+    baseURL: "https://booknest-backend-z703.onrender.com"
 });
 
 // ADD JWT TOKEN IN EVERY REQUEST
